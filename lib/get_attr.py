7f693c8dffad1aa7aea2ed6809a7ba22a772803c
PengfeiWangWZ <hehtihawa@gmail.com>
2016-11-09 12:41:44 -0500

    add for loop

--- lib/get_attr.py
+++ lib/get_attr.py
@@ -16,8 +16,12 @@ for root, dirs, files in os.walk(base_path):
 h5 = hdf5_getters.open_h5_file_read('/Users/pengfeiwang/Desktop/prj4/Project4_data/data/A/P/A/TRAPAES128E0786E60.h5')
 
 # get all the attr command 
-filter(lambda x: x[:3] == 'get',hdf5_getters.__dict__.keys())
+filter(lambda x: x[:3] == 'get', hdf5_getters.__dict__.keys())
 title = hdf5_getters.get_title(h5)
 
 
 
+duration = []
+for file in path_list:
+	file_read = hdf5_getters.open_h5_file_read(file)
+	duration.append(hdf5_getters.get_duration(file_read))
