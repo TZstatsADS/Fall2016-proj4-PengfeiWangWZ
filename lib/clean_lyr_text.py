2409e393e1c750d56294ad664c16f416690d68a1
PengfeiWangWZ <hehtihawa@gmail.com>
2016-11-10 19:28:15 -0500

    Bug fix

--- lib/clean_lyr_text.py
+++ lib/clean_lyr_text.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import ast
+import pickle
 
 with open('/Users/pengfeiwang/Desktop/prj4/Project4_data/mxm_dataset_train.txt','rb') as g:
 	dta = g.readlines()
@@ -9,9 +10,9 @@ dta_normal = [i for i in dta if not i.startswith(tuple(punc))]
 values = [i.split(',',2)[2:] for i in dta_normal]
 names = [i.split(',',1)[0] for i in dta_normal]
 values = [ast.literal_eval(''.join(("{",value[0],"}"))) for value in values]
-dictionary = dict(zip(name, values))
+dictionary = dict(zip(names, values))
 df = pd.DataFrame.from_dict(dictionary)
 df = pd.transpose(df)
 df = df.fillna(0)
 
-
+pickle.dump(df,open('/Users/pengfeiwang/Desktop/mxm_dataset_train.csv','w+'))
--- lib/lda_model.py
+++ lib/lda_model.py
@@ -1,8 +1,12 @@
 import scipy.sparse as sparse
+from scipy.stats import rankdata
 import pandas as pd
+import skfuzzy as fuzz
+import matplotlib.pyplot as plt
 import nltk
 from sklearn.decomposition import LatentDirichletAllocation
 from nltk.corpus import stopwords
+
 
 # print the top words
 def print_top_words(model, feature_names, n_top_words):
@@ -19,7 +23,7 @@ def data_clean(data_set):
 	col_names_clean = [i for i in data_set.columns if i not in stop_words]
 	data_set = data_set.ix[:,col_names_clean]
 	sparse_matrix = sparse.csr_matrix(data_set.values)
-	return(sparse_matrix)
+	return(sparse_matrix, col_names_clean)
 
 
 # load the data
@@ -30,19 +34,25 @@ print (dta == 0).sum().sum() / dta.shape[0] / float(dta.shape[1])
 
 
 # set the param
-n_topics=10
+n_topics = 500
 lda = LatentDirichletAllocation(n_topics=n_topics, max_iter=100, learning_method='online', learning_offset=50., random_state=2016)
 
-train = data_clean(dta)
-lda.fit_transform(train)  # 2350 * n_topics
+# dimention reduction via lda
+train, feature_names = data_clean(dta)
+new_train = lda.fit_transform(train)  # 2350 * n_topics
 
 # get the top words
-feature_names = dta.columns
 n_top_words = 20
 print_top_words(lda, feature_names, n_top_words)
 
+# fuzz c-means clustering
+n_centers = 10
+fuzziness_degree = 2
+error = 0.005
+maxiter = 1000
+centers, predict_prob, u0, d, jm, n_iters, fpc = fuzz.cluster.cmeans(new_train, c=n_centers, m=fuzziness_degree, error=error, maxiter=maxiter, init=None)
+# cluster_result = np.argmax(predict_prob, axis=0)
 
-
-
-
+prob = zip(*[i for i in predict_prob])
+prob = [list(i) for i in prob]
 
