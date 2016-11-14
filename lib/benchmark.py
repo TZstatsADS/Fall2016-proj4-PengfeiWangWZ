fd4baad70f53867ffc9099d0d3f7af6c78fc7635
PengfeiWangWZ <hehtihawa@gmail.com>
2016-11-14 17:52:36 -0500

    update

--- lib/benchmark.py
+++ lib/benchmark.py
@@ -1,9 +1,27 @@
 import pandas as pd
 import numpy as np
+import random
 from scipy.stats import rankdata
 
 dta = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/lyr_new.csv',index_col = ['track_id'])
 word_count = dta.apply(lambda x: x.sum())
 word_prop = word_count/float(sum(word_count))
-rank = rankdata(word_prop)
-np.savetxt('/Users/pengfeiwang/Desktop/benchmark.csv', rank, delimiter=",")
+rank_bm = rankdata(word_prop)
+# np.savetxt('/Users/pengfeiwang/Desktop/benchmark.csv', rank_bm, delimiter=",")
+
+# train-vali split
+seed = 2016
+random.seed(seed)
+s_index = dta.index.tolist()
+random.shuffle(s_index)
+train = s_index[235:]
+vali = s_index[:235]
+
+
+train = dta.loc[train]
+vali = dta.loc[vali]
+
+
+rank_vali = [rankdata(i) for i in vali.values]
+np.mean([sum(abs(i-rank_bm))/2350 for i in rank_vali])
+# 2577.6740117700319
