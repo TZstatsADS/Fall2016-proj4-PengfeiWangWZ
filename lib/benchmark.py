91fe5fb13c0284a99750601dae27af1ef6ca128b
PengfeiWangWZ <hehtihawa@gmail.com>
2016-11-17 15:44:51 -0500

    Update rank

--- lib/benchmark.py
+++ lib/benchmark.py
@@ -3,8 +3,8 @@ import numpy as np
 import random
 from scipy.stats import rankdata
 
-def calu_result(benchmark_df, test_data):
-	new_df = pd.concat([benchmark_df,test_data])
+def calu_result(pred_df, true_df):
+	new_df = pd.concat([pred_df,true_df])
 	# pd.concat([dist,vali.iloc[[0]]])
 	mean_rank = new_df.iloc[[0]].loc[:, new_df.iloc[1] != 0].sum(axis=1)[0]/sum(new_df.iloc[1] != 0)
 	return(mean_rank)
@@ -13,8 +13,14 @@ def calu_result(benchmark_df, test_data):
 dta = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/lyr_new.csv',index_col = ['track_id'])
 word_count = dta.apply(lambda x: x.sum())
 word_prop = word_count/float(sum(word_count))
-rank_bm = rankdata(word_prop)
+
+rank_bm = [4973]*4973 - rankdata(word_prop)
+a = pd.DataFrame(rank_bm).transpose()
+a.columns = dta.columns
+calu_result(a, dta.iloc[[0]])
+
 # np.savetxt('/Users/pengfeiwang/Desktop/benchmark.csv', rank_bm, delimiter=",")
+
 
 # train-vali split
 seed = 2016
@@ -37,8 +43,8 @@ dist.columns = train.columns
 a = []
 for i in range(vali.shape[0]):
 	b = calu_result(dist, vali.iloc[[i]])
-	print b
+	# print b
 	a.append(b)
 
-np.mean(a)/((1+5000)/2)
-# 1.73
+np.mean(a)/((4974)/2.)
+# 0.26078588698759286
--- lib/model.py
+++ lib/model.py
@@ -5,7 +5,7 @@ from utilities import *
 from scipy.stats import rankdata
 from scipy.cluster.vq import *
 from sklearn.multioutput import MultiOutputRegressor
-from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
 
 new_timbre = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/timbre.csv',index_col=['track_id'])
 new_pitch = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/pitch.csv',index_col=['track_id'])
@@ -14,10 +14,12 @@ x = pd.merge(new_timbre, new_pitch, left_index = True, right_index = True)
 x = pd.merge(x, stats, left_index = True, right_index = True)
 x[np.isnan(x)]=0
 
-y = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/lda_10.csv', index_col=['track_id'])
+y = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/lsyc_new.csv', index_col=['track_id'])
 
-clf = MultiOutputRegressor(RandomForestRegressor(random_state=0)).fit(x, y)
+clf = MultiOutputRegressor(GradientBoostingRegressor(random_state=0)).fit(x, y)
 pred = clf.predict(x)
+
+
 
 # test path_list
 dta = ls2df(path_list, selected_feature) 
@@ -25,17 +27,25 @@ codebook_timbre = pickle.load(open('/Users/pengfeiwang/Desktop/prj4/Project4_dat
 new_feature = get_sc(dta, codebook_timbre)
 
 
-pred_df = pd.DataFrame(pred)
-freq = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/freq.csv')
+clf = pickle.load(open('/Users/pengfeiwang/Desktop/prj4/Project4_data/clf.p','rb'))
+pred = clf.predict(x)
 
-result = pd.DataFrame()
-submit = pd.DataFrame()
-for j in range(len(path_list)):
-	for i in range(10):
-		result[i] = np.dot(pred_df.ix[j,i], freq.iloc[i]).tolist()
-	submit[path_list[j]] = rankdata(result.sum(axis=1).tolist())
+rank_bm = pd.DataFrame(index = range(100))
+for i in pred.shape[0]:
+	rank_bm.iloc[i] = [4973]*4973 - rankdata(pred.iloc[i])
 
-submit = pd.DataFrame.transpose(submit)
+
+# pred_df = pd.DataFrame(pred)
+# freq = pd.read_csv('/Users/pengfeiwang/Desktop/prj4/Project4_data/freq.csv')
+
+# result = pd.DataFrame()
+# submit = pd.DataFrame()
+# for j in range(len(path_list)):
+# 	for i in range(10):
+# 		result[i] = np.dot(pred_df.ix[j,i], freq.iloc[i]).tolist()
+# 	submit[path_list[j]] = rankdata(result.sum(axis=1).tolist())
+
+# submit = pd.DataFrame.transpose(submit)
 
 
 
